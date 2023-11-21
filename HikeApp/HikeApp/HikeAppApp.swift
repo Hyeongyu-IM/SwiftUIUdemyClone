a7ad4bf421a25c9b328f794749c28ffbd8c2d6f5
HyeonGyuIM <limku220@gmail.com>
2023-11-21 23:26:37 +0900

    initial Commit

---
+++ HikeApp/HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by 임현규 on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
