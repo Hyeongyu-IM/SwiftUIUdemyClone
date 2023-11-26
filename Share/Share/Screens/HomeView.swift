c1ee8f07834b0c4d9520e9dcf957b599bee2e1b0
HyeonGyuIM <limku220@gmail.com>
2023-11-26 18:55:42 +0900

    [Share] 개발 완료
    - Text뷰에 id를 부여해야하는 이유
    - 애니메이션 추가시 고려해야할 사항

--- Share/Share/Screens/HomeView.swift
+++ Share/Share/Screens/HomeView.swift
@@ -13,56 +13,58 @@ struct HomeView: View {
     @State private var isAnimating: Bool = false
     
     var body: some View {
-        VStack(spacing: 20, content: {
-            // MARK: -- Header
-            
-            Spacer()
-            
-            ZStack {
-                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+        ZStack {
+            Color.white
+                .ignoresSafeArea()
+            VStack(spacing: 20, content: {
+                // MARK: -- Header
                 
-                Image(.character2)
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-                    .offset(y: isAnimating ? 35 :  -35)
-                    .animation(.easeInOut(duration: 4).repeatForever(), value: isAnimating)
-            }
-            
-            
-            // MARK: -- Center
-            
-            Text("The time that leads to mastery is dependent on the intensity of our focus.")
-                .font(.title3)
-                .fontWeight(.light)
-                .foregroundStyle(.secondary)
-                .multilineTextAlignment(.center)
-                .padding()
-            
-            // MARK: -- Footer
-            
-            Spacer()
-            
-            Button(action: {
-                withAnimation {
-                    isOnboardingViewActive = true
+                Spacer()
+                
+                ZStack {
+                    CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                    
+                    Image(.character2)
+                        .resizable()
+                        .scaledToFit()
+                        .padding()
+                        .offset(y: isAnimating ? 35 :  -35)
+                        .animation(.easeInOut(duration: 4).repeatForever(), value: isAnimating)
                 }
-            }, label: {
-                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
-                    .imageScale(.large)
-                Text("Restart")
-                    .font(.system(.title3, design: .rounded))
-                    .fontWeight(.bold)
-            })//: Button
-            .buttonStyle(.borderedProminent)
-            .buttonBorderShape(.capsule)
-            .controlSize(.large)
-        }) //: VSTACK
-        .onAppear(perform: {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
-                isAnimating = true
+                
+                
+                // MARK: -- Center
+                
+                Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundStyle(.secondary)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                
+                // MARK: -- Footer
+                
+                Spacer()
+                
+                Button(action: {
+                    isOnboardingViewActive = true
+                }, label: {
+                    Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                        .imageScale(.large)
+                    Text("Restart")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                })//: Button
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.capsule)
+                .controlSize(.large)
+            }) //: VSTACK
+            .onAppear(perform: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                    isAnimating = true
+                })
             })
-        })
+        }
     }
 }
 
--- Share/Share/Screens/OnboardingView.swift
+++ Share/Share/Screens/OnboardingView.swift
@@ -14,6 +14,9 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
+    @State private var imageOffset: CGSize = .zero
+    @State private var indicatorOpacity: Double = 1.0
+    @State private var textTitle: String = "Share."
     
     var body: some View {
         ZStack(content: {
@@ -24,10 +27,12 @@ struct OnboardingView: View {
                 //MARK: -- Header
                 Spacer()
                 VStack(content: {
-                    Text("Share.")
+                    Text(textTitle)
                         .font(.system(size: 60))
                         .fontWeight(.heavy)
                         .foregroundStyle(.white)
+                        .transition(.opacity)
+                        .id(textTitle)
                     
                     Text("""
 It's not how much we give but how much love we put into giving.
@@ -45,13 +50,49 @@ It's not how much we give but how much love we put into giving.
                 
                 ZStack(content: {
                     CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                        .offset(x: imageOffset.width * -1)
+                        .blur(radius: abs(imageOffset.width / 5))
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                     
                     Image(.character1)
                         .resizable()
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeInOut(duration: 0.5), value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2, y: 0)
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    print("imageOffset.width \(gesture.translation)")
+                                    if abs(imageOffset.width) <= 150 {
+                                        imageOffset = gesture.translation
+                                        
+                                        withAnimation(.linear(duration: 0.25)) {
+                                            indicatorOpacity = 0
+                                            textTitle = "Give."
+                                        }
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    imageOffset = .zero
+                                    withAnimation(.linear(duration: 0.25)) {
+                                        indicatorOpacity = 1
+                                        textTitle = "Share."
+                                    }
+                                })
+                        )
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                 }) //: Center
+                .overlay(alignment: .bottom) {
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .ultraLight))
+                        .foregroundStyle(.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                        .opacity(indicatorOpacity)
+                }
                 
                 Spacer()
                 
@@ -101,14 +142,12 @@ It's not how much we give but how much love we put into giving.
                                     }
                                 })
                                 .onEnded { _ in
-                                    withAnimation(.easeOut(duration: 0.4), {
-                                        if buttonOffset > buttonWidth / 2 {
-                                            buttonOffset = buttonWidth - 80
-                                            isOnboardingViewActive = false
-                                        } else {
-                                            buttonOffset = 0
-                                        }
-                                    })
+                                    if buttonOffset > buttonWidth / 2 {
+                                        buttonOffset = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    } else {
+                                        buttonOffset = 0
+                                    }
                                 }
                         )//: Gesture
                         
