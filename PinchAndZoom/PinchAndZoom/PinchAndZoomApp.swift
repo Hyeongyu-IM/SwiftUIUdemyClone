98639c6c493b361c681bad75e3d33fafad33602d
HyeonGyuIM <limku220@gmail.com>
2023-11-26 21:36:33 +0900

    [PinchAndZoom] Create Project

---
+++ PinchAndZoom/PinchAndZoom/PinchAndZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAndZoomApp.swift
+//  PinchAndZoom
+//
+//  Created by 임현규 on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchAndZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
