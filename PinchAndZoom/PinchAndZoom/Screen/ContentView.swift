30b1615f9c62c202604d0b90bc89fed46e52c4ba
HyeonGyuIM <limku220@gmail.com>
2023-11-26 23:24:03 +0900

    [Pinch] DragAndDrop, TapGesture

--- PinchAndZoom/PinchAndZoom/Screen/ContentView.swift
+++ PinchAndZoom/PinchAndZoom/Screen/ContentView.swift
@@ -8,17 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    func resetImageState() {
+        return withAnimation(.spring(.bouncy)) {
+            imageScale = 1
+            imageOffset = .zero
         }
-        .padding()
+    }
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Image(.thumbMagazineFrontCover)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
+                    .scaleEffect(imageScale)
+                //MARK: -- 1. Tap Gesture
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring(.bouncy)) {
+                                imageScale = 5
+                            }
+                        } else {
+                            resetImageState()
+                        }
+                    })
+                //MARK: -- 2. Drag Gesture
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            })
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
+            }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            })
+        }
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
